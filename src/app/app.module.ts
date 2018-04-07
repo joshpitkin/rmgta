1889dfd5762d6418a077711323c15f700a3e14f7
Josh Pitkin (jpitkin) [IMFT] <jpitkin@micron.com>
2018-04-06 22:15:21 -0600

    initial version with actual content

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
+import { CarouselModule } from 'ngx-bootstrap/carousel';
 import { AppComponent } from './app.component';
 
 
@@ -10,7 +10,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CarouselModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
